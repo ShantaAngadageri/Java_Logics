786b9bbf1fc62fd50f35e89a7ea072060f48a19c
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-27 22:15:37 +0530

    Add files via upload

---
+++ Normalize.java
@@ -0,0 +1,20 @@
+class Normalize
+{
+	public static void main(String [] args)
+	{
+		String str=" John   Doe0123 ";
+		//Validation
+		if(str.length()>=6 && str.length<=20)
+		{
+			if
+			
+			//System.out.println("Length must be 6-20 characters");
+		if(str.startswith(A-Za-z)
+		str=str.toLowerCase();
+		str=str.trim();
+		str=str.replaceAll(" +","_");
+		str=str.replaceAll("[^a-z0-9_]", "");
+		
+		System.out.println(str);
+	}
+}
