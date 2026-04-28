7f2018ad3342b52860e82c5cd21788d586b54352
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-28 13:07:11 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,20 @@
+class one{
+    public static void main(String[] args){
+        int n=4;
+        for(int row=1;row<=n;row++){
+            // for(int col=1;col<=row;col++){
+            //     System.out.print("*");
+            // }
+           for(int col=1; col<=n;col++){
+            if(row>=col){
+                System.out.print("*"); 
+            }
+            else{
+                System.out.print(" ");
+            }
+           }
+
+            System.out.println();
+        }
+    }
+}
