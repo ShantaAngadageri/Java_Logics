76bd0b58d6e2b95ac63c790ea92a864925cb67ce
ShantaAngadageri <shantaangadageri@gmail.com>
2026-04-28 13:05:03 +0530

    Add files via upload

---
+++ Sample8.java
@@ -0,0 +1,17 @@
+
+/*
+public class Sample8 {
+     public static void main(String[] args) {
+    	 String s1="Pritam";
+    	 s1=s1.replace('i', 'a');
+    	 System.out.println(s1);
+     }
+} */
+
+public class Sample8 {
+     public static void main(String[] args) {
+    	 String s1="methodOverloading";
+    	 s1=s1.replaceAll("a",");
+    	 System.out.println(s1);
+     }
+}
